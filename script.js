90d858e0eb124f2a22f4c7629de6b3b98693d1c4
Soulistic <smfaizalam786@gmail.com>
2023-10-06 22:10:31 +0530

    fixed no city found userInfo bug

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
 
-//initially vairables need????
-
 let oldTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 oldTab.classList.add("current-tab");
@@ -21,37 +19,28 @@ function switchTab(newTab) {
         oldTab.classList.add("current-tab");
 
         if(!searchForm.classList.contains("active")) {
-            //kya search form wala container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
         }
         else {
-            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
-            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
-            //for coordinates, if we haved saved them there.
             getfromSessionStorage();
         }
     }
 }
 
 userTab.addEventListener("click", () => {
-    //pass clicked tab as input paramter
     switchTab(userTab);
 });
 
 searchTab.addEventListener("click", () => {
-    //pass clicked tab as input paramter
     switchTab(searchTab);
 });
-
-//check if cordinates are already present in session storage
 function getfromSessionStorage() {
     const localCoordinates = sessionStorage.getItem("user-coordinates");
     if(!localCoordinates) {
-        //agar local coordinates nahi mile
         grantAccessContainer.classList.add("active");
     }
     else {
@@ -63,9 +52,7 @@ function getfromSessionStorage() {
 
 async function fetchUserWeatherInfo(coordinates) {
     const {lat, lon} = coordinates;
-    // make grantcontainer invisible
     grantAccessContainer.classList.remove("active");
-    //make loader visible
     loadingScreen.classList.add("active");
 
     //API CALL
@@ -83,8 +70,7 @@ async function fetchUserWeatherInfo(coordinates) {
     }
     catch(err) {
         loadingScreen.classList.remove("active");
-        //HW
-
+        console.log(err);
     }
 
 }
@@ -182,13 +168,14 @@ async function fetchSearchWeatherInfo(city) {
         if (data.cod === "404") {
             // Display an alert if no city or country is found
             alert("City or country not found. Please check your input.");
+            loadingScreen.classList.remove("active");
+            userInfoContainer.classList.remove("active");
         } else {
             // Render weather info if data is available
             renderWeatherInfo(data);
         }
     }
     catch(err) {
-        //hW
         console.error(err);
     }
 }
